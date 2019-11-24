47cdc66c1e1dff64e004124b61f380d1c97788c6
Najeeb <najeebq@gmail.com>
2019-11-23 19:20:45 -0800

    Added Spakr code for dataframes processing and making model

--- model/modelPredict.py
+++ model/modelPredict.py
@@ -1,11 +1,12 @@
+import tensorflow as tf
+import tensorflow_hub as hub
 from keras.preprocessing.text import Tokenizer
 from keras.preprocessing.sequence import pad_sequences
 from keras.models import Sequential
 from keras.layers import Dense, Flatten, LSTM, Conv1D, MaxPooling1D, Dropout, Activation
 from keras.layers.embeddings import Embedding
 import pickle
-
-
+import pandas as pd
 
 
 def main():
