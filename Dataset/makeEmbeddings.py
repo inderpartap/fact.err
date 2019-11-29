0d6dc92c26bcfb2a2fcbd38732608084bc40a330
Najeeb <najeebq@gmail.com>
2019-11-28 18:15:32 -0800

    Adding MLP Classifier for data frame processing and also code for distributed deep learning using Elephas

--- Dataset/makeEmbeddings.py
+++ Dataset/makeEmbeddings.py
@@ -3,6 +3,7 @@
 # @Date:   2019-11-23 18:31:15
 # @Last Modified by:   najeebq
 # @Last Modified time: 2019-11-23 19:09:49
+
 import nltk
 from nltk.corpus import stopwords
 from nltk.stem import WordNetLemmatizer
@@ -15,21 +16,40 @@ import numpy as np
 import re
 import sys
 import tensorflow as tf
-import tensorflow_hub as hub
+#import tensorflow_hub as hub
 import os
-from keras.preprocessing.text import Tokenizer
-from keras.preprocessing.sequence import pad_sequences
+# from keras.preprocessing.text import Tokenizer
+# from keras.preprocessing.sequence import pad_sequences
 from keras.models import Sequential
-from keras.layers import Dense, Flatten, LSTM, Conv1D, MaxPooling1D, Dropout, Activation
-from keras.layers.embeddings import Embedding
+# from keras.layers import Dense, Flatten, LSTM, Conv1D, MaxPooling1D, Dropout, Activation
+# from keras.layers.embeddings import Embedding
+from keras.layers import Dense, Dropout, Activation,Embedding,LSTM
+from keras.utils import np_utils, generic_utils
+from keras import optimizers
 
-assert sys.version_info >= (3, 5) # make sure we have Python 3.5+
+from elephas.utils.rdd_utils import to_simple_rdd
+from elephas.ml_model import ElephasEstimator
+from elephas.spark_model import SparkModel
+#assert sys.version_info >= (3, 5) # make sure we have Python 3.5+
 from pyspark.sql import SparkSession, functions, types
 from pyspark.sql.functions import udf
+from pyspark.ml.feature import Word2Vec
+from pyspark.ml import Pipeline
+from pyspark.mllib.evaluation import MulticlassMetrics
+
+
+import tensorflow.contrib.eager as tfe
+tf.enable_eager_execution()
+
+
 spark = SparkSession.builder.appName('DF processing').getOrCreate()
-assert spark.version >= '2.3' # make sure we have Spark 2.3+
+#assert spark.version >= '2.3' # make sure we have Spark 2.3+
 spark.sparkContext.setLogLevel('WARN')
 sc = spark.sparkContext
+
+#from pyspark import SparkContext, SparkConf
+#conf = SparkConf().setAppName('Elephas_App').setMaster('local[8]')
+#sc = SparkContext(conf=conf)
 
 
 
@@ -37,58 +57,116 @@ sc = spark.sparkContext
 #make glove embeddings using Dict
 #fit data
 #make word vectors
+#tf.enable_eager_execution()
 
 
-def main(fileinput):
+def main():
 
 	data_schema = types.StructType([types.StructField('title', types.StringType()),types.StructField('text', types.StringType()),types.StructField('label', types.IntegerType())])
 
-	datadf = spark.read.csv(fileinput,schema=data_schema)
+	datadf = spark.read.csv("s3://projfakenews/ProcessedDatawithoutStemming",schema=data_schema)
 
-	embeddings_index = make_embeddings()
+	#embeddings_index = make_embeddings()
 
 	#reading and tokenizing
-	vocabulary_size = 2000
-	tokenizer = Tokenizer(num_words= vocabulary_size)
+	#vocabulary_size = 2000
 
-	textDF = datadf.select("text")
-
-	tokenizer.fit_on_texts(textDF)
+	# tokenizer   = Tokenizer()
+	#textDF = datadf.select("text")
+	# tokenizer.fit_on_texts(textDF)
+	# datadf.show()
 	datadf.show()
 
-	tokenizer.word_index.items()
+	word2Vec = Word2Vec(vectorSize=100, minCount=0, inputCol="text", outputCol="features")
+	datadf = datadf.withColumn("text", functions.array("text"))
+	model = word2Vec.fit(datadf)
+	result = model.transform(datadf)
 
-	embedding_matrix = np.zeros((vocabulary_size, 100))
-	for word, index in tokenizer.word_index.items():
-		if index > vocabulary_size - 1:
-			break
-		else:
-			embedding_vector = embeddings_index.get(word)
-			if embedding_vector is not None:
-				embedding_matrix[index] = embedding_vector
+	result.show()
 
-	sequences = tokenizer.texts_to_sequences(kaggle_train['text'])
+	temp = result.select("features").show(10)
+	result.dropna()
+
+	make_model(result)
 
 	#save tokenizer 
-	with open('tokenizer.pickle', 'wb') as handle:
-		pickle.dump(tokenizer, handle, protocol=pickle.HIGHEST_PROTOCOL)
-	data = pad_sequences(sequences, maxlen=50)
+	# with open('tokenizer.pickle', 'wb') as handle:
+	# 	pickle.dump(tokenizer, handle, protocol=pickle.HIGHEST_PROTOCOL)
+	
 
 
 
 
 
-def make_model():
-	model_glove = Sequential()
-	model_glove.add(Embedding(vocabulary_size, 50, input_length=50, weights=[embedding_matrix], trainable=False))
-	model_glove.add(Dropout(0.2))
-	model_glove.add(Conv1D(64, 5, activation='relu'))
-	model_glove.add(MaxPooling1D(pool_size=4))
-	model_glove.add(LSTM(100))
-	model_glove.add(Dense(1, activation='sigmoid'))
-	model_glove.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])## Fit train data
-	model_glove.fit(data, np.array(labels), validation_split=0.4, epochs = 3)
-	model_glove.save('fakenewsClassifier') 
+def make_model(data):
+	data.show()
+	data = data.dropna()
+	nb_classes = data.select("label").distinct().count()
+	input_dim = len(data.select("features").first()[0])
+
+	print(nb_classes,input_dim)
+
+	model = Sequential()
+	model.add(Embedding(input_dim=input_dim,output_dim=100))
+	#model.add(LSTM(64,return_sequences=False,dropout=0.1,recurrent_dropout=0.1))
+	model.add(Dense(100,activation='relu'))
+	model.add(Dropout(0.5))
+	model.add(Dense(nb_classes,activation='softmax'))
+	#sgd = optimizers.SGD(lr=0.1)
+	#model.compile(sgd, 'categorical_crossentropy', ['acc'])
+	model.compile(loss ='binary_crossentropy', optimizer='adam')
+
+	#model.compile(loss='categorical_crossentropy', optimizer='adam')
+	spark_model = SparkModel(model, frequency='epoch', mode='asynchronous')
+
+	
+	
+
+	adam = optimizers.Adam(lr=0.01)
+	opt_conf = optimizers.serialize(adam)
+
+	estimator = ElephasEstimator()
+	estimator.setFeaturesCol("features")
+	estimator.setLabelCol("label")
+	estimator.set_keras_model_config(model.to_yaml())
+	estimator.set_categorical_labels(True)
+	estimator.set_nb_classes(nb_classes)
+	estimator.set_num_workers(1)
+	estimator.set_epochs(20) 
+	estimator.set_batch_size(128)
+	estimator.set_verbosity(1)
+	estimator.set_validation_split(0.15)
+	estimator.set_optimizer_config(opt_conf)
+	estimator.set_mode("synchronous")
+	estimator.set_loss("categorical_crossentropy")
+	estimator.set_metrics(['acc'])
+
+
+	#estimator = ElephasEstimator(model, epochs=20, batch_size=32, frequency='batch', mode='asynchronous', nb_classes=1)
+
+	pipeline = Pipeline(stages=[estimator])
+	#fitted_model = estimator.fit(data)
+	#prediction = fitted_model.transform(data)
+	
+	fitted_pipeline = pipeline.fit(data) # Fit model to data
+	prediction = fitted_pipeline.transform(data) # Evaluate on train data.
+	# prediction = fitted_pipeline.transform(test_df) # <-- The same code evaluates test data.
+	pnl = prediction.select("text", "prediction")
+	pnl.show(100)
+
+	prediction_and_label = pnl.map(lambda row: (row.text, row.prediction))
+	metrics = MulticlassMetrics(prediction_and_label)
+	print(metrics.precision())
+	pnl = prediction.select("label", "prediction").show()
+	pnl.show(100)
+
+	#prediction_and_label= pnl.rdd.map(lambda row: (row.label, row.prediction))
+	#metrics = MulticlassMetrics(prediction_and_label)
+	#print(metrics.precision())
+	#print(metrics.recall())
+	#spark_model.fit(rdd, epochs=20, batch_size=32, verbose=0, validation_split=0.1)
+	#spark_model.fit(data, np.array(labels), validation_split=0.4, epochs = 3)
+	#model_glove.save('fakenewsClassifier') 
 
 def make_embeddings():
 	embeddings_index = dict()
@@ -104,8 +182,8 @@ def make_embeddings():
 
 
 if __name__ == '__main__':
-    fileinput = sys.argv[1]
-    main(fileinput)
+    #fileinput = sys.argv[1]
+    main()
 
 
 
